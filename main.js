c6842dab7eb6a85314249253e1187f9d9634a1a5
Krishnaveni <veniumabala@gmail.com>
2022-12-26 09:14:37 +0000

    add

---
+++ main.js
@@ -0,0 +1,16 @@
+						/*TEAM*/
+						
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        
+    }
+})			
